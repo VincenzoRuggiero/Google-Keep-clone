bd7d79c296a672eefee3fb0477e4005089edf41b
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-09-12 11:17:35 +0200

    Create new note logic

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 class App {
   constructor() {
+    this.notes = [];
+
     this.$form = document.querySelector("#form");
     this.$noteTitle = document.querySelector("#note-title");
+    this.$noteText = document.querySelector("#note-text");
     this.$formButtons = document.querySelector("#form-buttons");
 
     this.addEventListeners();
@@ -11,7 +14,19 @@ class App {
     document.body.addEventListener("click", (event) => {
       this.handleFormClick(event);
     });
+
+    this.$form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const title = this.$noteTitle.value;
+      const text = this.$noteText.value;
+      const hasNote = title || text;
+
+      if (hasNote) {
+        this.addNote({ title, text });
+      }
+    });
   }
+
   handleFormClick(event) {
     const isFormClicked = this.$form.contains(event.target);
 
@@ -21,15 +36,28 @@ class App {
       this.closeForm();
     }
   }
+
   openForm() {
     this.$form.classList.add("form");
     this.$noteTitle.style.display = "block";
     this.$formButtons.style.display = "block";
   }
+
   closeForm() {
     this.$form.classList.remove("form");
     this.$noteTitle.style.display = "none";
     this.$formButtons.style.display = "none";
+  }
+
+  addNote(note) {
+    const newNote = {
+      title: note.title,
+      text: note.text,
+      color: "white",
+      id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
+    };
+
+    this.notes = [...this.notes, newNote];
   }
 }
 
