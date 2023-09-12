ce00146de8cb6eb912f776ccd30b16e8613ce221
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-09-12 10:59:13 +0200

    Add events to open/close the input field

---
+++ app.js
@@ -0,0 +1,36 @@
+class App {
+  constructor() {
+    this.$form = document.querySelector("#form");
+    this.$noteTitle = document.querySelector("#note-title");
+    this.$formButtons = document.querySelector("#form-buttons");
+
+    this.addEventListeners();
+  }
+
+  addEventListeners() {
+    document.body.addEventListener("click", (event) => {
+      this.handleFormClick(event);
+    });
+  }
+  handleFormClick(event) {
+    const isFormClicked = this.$form.contains(event.target);
+
+    if (isFormClicked) {
+      this.openForm();
+    } else {
+      this.closeForm();
+    }
+  }
+  openForm() {
+    this.$form.classList.add("form");
+    this.$noteTitle.style.display = "block";
+    this.$formButtons.style.display = "block";
+  }
+  closeForm() {
+    this.$form.classList.remove("form");
+    this.$noteTitle.style.display = "none";
+    this.$formButtons.style.display = "none";
+  }
+}
+
+new App();
